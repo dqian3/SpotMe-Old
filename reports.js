83b0ca6989c8471e422f8b589eac5a42a947377b
Nathan Kaleida <nkaleida@gmail.com>
2017-09-16 16:48:05 -0400

    Report page backend done

---
+++ reports.js
@@ -0,0 +1,72 @@
+var config = {
+    apiKey: "AIzaSyCUjMnWd8szLkDenWZV58BWfmRq0Sm7dMI",
+    authDomain: "spotme-a2502.firebaseapp.com",
+    databaseURL: "https://spotme-a2502.firebaseio.com",
+    projectId: "spotme-a2502",
+    storageBucket: "spotme-a2502.appspot.com",
+    messagingSenderId: "1041315357650"
+  };
+  firebase.initializeApp(config);
+
+
+var userId= "4HRB2yLjjCbeeKQmMfodjIFSBc12"; ///these will change with implementation
+var reportedId= "SwUOBxSqBqgyYH4znoH23BbE8px1";//////^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+function fileReport(id_of_reported, id_of_reporter)
+{
+	console.log("im in");
+	var time = new Date;
+	var preferred;
+
+	if(document.getElementById('phone').checked) 
+	{
+  		preferred = "Phone";
+	}
+	else if(document.getElementById('email').checked) 
+	{
+		preferred = "Email";
+	}
+	else
+	{
+		preferred = "Email";
+	}
+	
+	var x = firebase.database().ref("reports/" + id_of_reported);
+	
+	var info = {
+				reporter: id_of_reporter,
+				time: time,
+				message: $("#message").val(),
+				preferred_method: preferred	
+			};
+	x.set( info	);
+	
+}
+
+function getReportInfo()
+{
+	firebase.database().ref('/orders/' + userId).once("value").then(function(deliverer) {
+  		var delivererId = deliverer.val().deliverer;
+  		//delivererId, in this case, is the person being reported
+  		console.log(delivererId);
+		
+		firebase.database().ref('/deliverers/').once("value").then(function(deliverer) {
+  			var recieverID = deliverer.child(reportedId).val();
+  			//recieverId is the person reporting
+  			console.log(recieverID);
+  			fileReport(delivererId, recieverID);
+  		});
+  	});
+
+  	
+
+}
+
+//getInfo(); ///remember to comment this out
+
+
+
+
+
+
+
