4102fd5263f7207afe173ee987f8769a5f193eae
Dan Qian <daniel.j.qian@gmail.com>
2017-09-16 14:40:32 -0400

    order_info loads data

--- order_info.js
+++ order_info.js
@@ -1,4 +1,4 @@
-var config = {
+ var config = {
     apiKey: "AIzaSyCUjMnWd8szLkDenWZV58BWfmRq0Sm7dMI",
     authDomain: "spotme-a2502.firebaseapp.com",
     databaseURL: "https://spotme-a2502.firebaseio.com",
@@ -6,7 +6,7 @@ var config = {
     storageBucket: "spotme-a2502.appspot.com",
     messagingSenderId: "1041315357650"
   };
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
 
 
 function store_order(order)
@@ -38,7 +38,7 @@ function grabData()
 		amount_needed: $("#amt_needed").val(),
 		max_eta: $("#max_eta_away").val(),
 		time: new Date(),
-		//location: 
+		location: {lat:user_latitude1, long: user_longitude1}, 
 		started: false
 		//ETA:
 	}
