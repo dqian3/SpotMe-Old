d5c87978ddb90595cae76cb3c655f704b70be0c9
Nathan Kaleida <nkaleida@gmail.com>
2017-09-16 02:35:55 -0400

    Order information page (with firebase)

--- order_info.js
+++ order_info.js
@@ -1,14 +1,32 @@
-function uploadData(order_data)
+var config = {
+    apiKey: "AIzaSyCUjMnWd8szLkDenWZV58BWfmRq0Sm7dMI",
+    authDomain: "spotme-a2502.firebaseapp.com",
+    databaseURL: "https://spotme-a2502.firebaseio.com",
+    projectId: "spotme-a2502",
+    storageBucket: "spotme-a2502.appspot.com",
+    messagingSenderId: "1041315357650"
+  };
+  firebase.initializeApp(config);
+
+
+function store_order(order)
 {
-	// Gives firebase all the data
+	var userId = firebase.auth().currentUser.uid;
+	firebase.database().ref('orders/' + order_info).set(order);
+
 }
+
 
 function grabData()
 {
 	var order = {
 		amount_needed: $("#amt_needed").val(),
 		max_dist: $("#max_dist_away").val(),
+		time: new Date(),
+		//location: 
+		started: false
+		//ETA:
 	}
 	
-	uploadData(order);
+	store_order(order);
 }
