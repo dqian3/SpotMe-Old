bb583382529b129afc93cdf16f2d128b308d3be9
Nathan Kaleida <nkaleida@gmail.com>
2017-09-16 01:52:09 -0400

    Order information page (pre-firebase, not pretty)

---
+++ order_info.js
@@ -0,0 +1,14 @@
+function uploadData(order_data)
+{
+	// Gives firebase all the data
+}
+
+function grabData()
+{
+	var order = {
+		amount_needed: $("amt_needed").val(),
+		max_dist: $("max_dist_away").val(),
+	}
+	
+	uploadData(order);
+}
