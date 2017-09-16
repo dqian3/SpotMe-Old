4c77c4311cb302df66608ba2151662bb6c3399c3
Dan Qian <daniel.j.qian@gmail.com>
2017-09-16 01:34:10 -0400

    User account creation

--- signup.js
+++ signup.js
@@ -1,21 +1,34 @@
-function uploadData(cust_data)
-{
-	// Gives firebase all the data
-}
+var config = {
+	apiKey: "AIzaSyCUjMnWd8szLkDenWZV58BWfmRq0Sm7dMI",
+	authDomain: "spotme-a2502.firebaseapp.com",
+	databaseURL: "https://spotme-a2502.firebaseio.com",
+	projectId: "spotme-a2502",
+	storageBucket: "spotme-a2502.appspot.com",
+	messagingSenderId: "1041315357650"
+};
+firebase.initializeApp(config);
+
 function store_cust_id(id) 
 {
 	//store in firebase
 	create_account(id);
 
+	var userId = firebase.auth().currentUser.uid;
+	firebase.database().ref('users/' + userId + "/cust_id").set(id);
+
 }
 
 function store_acc_id(id)
 {
-	//store in firebase
+	firebase.database().ref('users/' + userId + "/acc_id").set(id);
 }
 
 function grabData()
 {
+	if ($("pass").val() != $("confpass").val()) {
+		alert("Passwords don't match");
+		return;
+	}
 	var data = {
 		first_name: $("fname").val();
 		last_name: $("lname").val();
@@ -28,6 +41,13 @@ function grabData()
 			zip: $("zip").val();
 		}
 	}
+
+	firebase.auth().createUserWithEmailAndPassword($("email").val(), $("pass").val());
+
+	var userId = firebase.auth().currentUser.uid;
+	firebase.database().ref('users/' + userId).set({
+		personal_info: data;
+	});
+
 	register_customer(data);
-	uploadData(data);
 }
